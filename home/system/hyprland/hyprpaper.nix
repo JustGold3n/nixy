658bae59ee9a841635c85ee6c5de79da40316b41
JustGold3n <marek@pavlicek.eu>
2026-09-17 09:47:49 +0200

    Revert main to 80b2820

--- home/system/hyprland/hyprpaper.nix
+++ home/system/hyprland/hyprpaper.nix
@@ -4,10 +4,8 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [",/home/gold3n/.config/nixos/hosts/laptop/wallpaper1.jpg"];
-      wallpaper = [",/home/gold3n/.config/nixos/hosts/laptop/wallpaper1.jpg"];
-      # ipc = "on";
-      # splash = false;
+      ipc = "on";
+      splash = false;
     };
   };
   systemd.user.services.hyprpaper.Unit.After = lib.mkForce "graphical-session.target";
